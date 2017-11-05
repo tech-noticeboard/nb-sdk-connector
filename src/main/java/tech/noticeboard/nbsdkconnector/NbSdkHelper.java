497214b45145af3514c199b93efc2f6af95462d7
DESKTOP-4KG0EGG\Priyansh Srivastava <priyansh@noticeboard.tech>
2017-11-05 13:37:50 +0530

    SDKConnector: code to get user activity

---
+++ src/main/java/tech/noticeboard/nbsdkconnector/NbSdkHelper.java
@@ -0,0 +1,129 @@
+package tech.noticeboard.nbsdkconnector;
+
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+import com.google.i18n.phonenumbers.NumberParseException;
+import com.google.i18n.phonenumbers.PhoneNumberUtil;
+import com.google.i18n.phonenumbers.Phonenumber;
+
+import java.util.regex.Pattern;
+
+/**
+ * Created by Priyansh Srivastava on 31-Oct-17.
+ */
+
+class NbSdkHelper {
+
+    static String getLoginToken(Context context) {
+
+        Cursor c = null;
+        String token = "";
+
+        try {
+            String URL = Constants.CONTENT_PROVIDER_URL;
+
+            Uri uri = Uri.parse(URL);
+            c = context.getContentResolver().query(uri,
+                    null, null,null, Constants.CONTENT_PROVIDER_KEY);
+            if (null != c && c.moveToFirst()) {
+                do {
+                    token = c.getString(c.getColumnIndex(Constants.CONTENT_PROVIDER_KEY));
+                } while (c.moveToNext());
+            }
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        finally {
+            if(null != c) {
+                c.close();
+            }
+        }
+
+        return token;
+    }
+
+
+    public static String getCountryCode(Context context) {
+
+        String countryId = "";
+
+        try {
+            TelephonyManager manager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+
+            if (manager != null && manager.getSimCountryIso() != null) {
+                //getNetworkCountryIso
+                countryId = manager.getSimCountryIso().toUpperCase().trim();
+            }
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        finally {
+            if(countryId.isEmpty()) {
+                countryId = "+91";
+            }
+        }
+
+        return countryId;
+    }
+
+
+    public static String formatPhone(Context context, String phone) {
+
+        PhoneNumberUtil phoneUtil = PhoneNumberUtil.getInstance();
+        Phonenumber.PhoneNumber phoneNumber = null;
+        try {
+            phoneNumber = phoneUtil.parse(phone, getCountryCode(context));
+        } catch (NumberParseException e) {
+            System.err.println("NumberParseException was thrown: " + e.toString());
+        }
+        if(phoneNumber != null && phoneUtil.isValidNumber(phoneNumber)){
+            return phoneUtil.format(phoneNumber, PhoneNumberUtil.PhoneNumberFormat.E164);
+        }
+        return null;
+    }
+
+
+    public static boolean validateEmail(String email){
+        if(email == null)
+            return false;
+        return email.matches(Constants.EMAIL_REGEX);
+    }
+
+    public static boolean validatePhone(String phone) {
+        if(phone == null)
+            return false;
+        return Pattern.matches(Constants.PHONE_REGEX, phone);
+    }
+
+
+    public static String readSdkKey(@NonNull Context context) throws NullPointerException {
+
+        String TAG = "Noticeboard Helper";
+        String sdkKey = "";
+
+        try {
+            ApplicationInfo ai = context.getPackageManager().getApplicationInfo(
+                    context.getPackageName(), PackageManager.GET_META_DATA);
+            Bundle bundle = ai.metaData;
+            sdkKey = bundle.getString(Constants.MANIFEST_SDK_KEY);
+            if(null == sdkKey || sdkKey.isEmpty()) {
+                throw new NullPointerException();
+            }
+
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.e(TAG, "Failed to load meta-data, NameNotFound: " + e.getMessage());
+        }
+
+        return sdkKey;
+    }
+}
---
+++ src/main/java/tech/noticeboard/nbsdkconnector/nbUserActivity/UserActivityAsyncTask.java
@@ -0,0 +1,132 @@
+package tech.noticeboard.nbsdkconnector.nbUserActivity;
+
+/**
+ * Created by Priyansh Srivastava on 09-Oct-17.
+ */
+
+import android.os.AsyncTask;
+import android.util.JsonReader;
+import android.util.Log;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import tech.noticeboard.nbsdkconnector.Constants;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * Created by Priyansh on 24-10-2017.
+ */
+public class UserActivityAsyncTask extends AsyncTask<String, Void, Integer> {
+
+    private NBUserActivity userActivityInterface;
+
+    public void setup(NBUserActivity callingActivity) {
+        this.userActivityInterface = callingActivity;
+    }
+
+    @Override
+    protected Integer doInBackground(String... arg0) {
+
+        Integer result = -1;
+        HttpsURLConnection urlConnection = null;
+
+        try {
+
+            URL url = new URL(Constants.ACTIVITY_URL);
+            urlConnection = (HttpsURLConnection) url.openConnection();
+
+            urlConnection.setConnectTimeout(20000);
+            urlConnection.setReadTimeout(20000);
+            //urlConnection.setDoOutput(true);
+            urlConnection.setUseCaches(false);
+            urlConnection.setRequestMethod("GET");
+
+            String authorization = "Bearer " + arg0[0];
+            urlConnection.addRequestProperty("Authorization", authorization);
+            urlConnection.addRequestProperty("Content-Type", "application/json; charset=UTF-8");
+            urlConnection.addRequestProperty("Accept", "application/json");
+            urlConnection.connect();
+
+            int statusCode = urlConnection.getResponseCode();
+            if (statusCode != HttpURLConnection.HTTP_OK) {
+
+                Log.d(TAG, "NbSdkConnector:  connection failed: statusCode: " + statusCode);
+                result = -1;
+
+            } else {
+
+                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                InputStreamReader responseBodyReader = new InputStreamReader(in, "UTF-8");
+                result = getActivityCountFromJson(responseBodyReader);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+        }
+
+        return result;
+    }
+
+
+    private static int getActivityCountFromJson(InputStreamReader reader) {
+
+        int activityCount = -1;
+
+        try {
+
+            String value = "";
+
+            JsonReader jsonReader = new JsonReader(reader);
+            jsonReader.beginObject(); // Start processing the JSON object
+            while (jsonReader.hasNext()) { // Loop through all keys
+                String key = jsonReader.nextName(); // Fetch the next key
+                if (key.equals(Constants.ACTIVITY_COUNT_KEY)) { // Check if desired key
+                    // Fetch the value as a String
+                    value = jsonReader.nextString();
+                    break; // Break out of the loop
+                } else {
+                    jsonReader.skipValue(); // Skip values of other keys
+                }
+            }
+
+            //Close the json reader
+            jsonReader.close();
+
+            //parse the value
+            if(null != value && !value.isEmpty()) {
+                activityCount = Integer.parseInt(value);
+            }
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return activityCount;
+    }
+
+
+    @Override
+    protected void onPostExecute(Integer activityCount) {
+        switch (activityCount) {
+            case -1:    userActivityInterface.activityError(0, "Error");
+                        break;
+            case 0:     userActivityInterface.noActivity();
+                        break;
+            default:
+                userActivityInterface.activityPresent(activityCount);
+                break;
+        }
+    }
+}
